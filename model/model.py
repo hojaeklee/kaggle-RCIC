c22cdec9c4bd852927eff9bc0d8f890a8e3a96db
hojaelee@umich.edu <hojaelee@gl-login1.arc-ts.umich.edu>
2019-08-19 10:53:12 -0400

    Add ResNet152 model

--- model/model.py
+++ model/model.py
@@ -104,7 +104,7 @@ class DenseNet201(BaseModel):
         preloaded = torchvision.models.densenet201(pretrained = True)
         self.features = preloaded.features
         self.features.conv0 = nn.Conv2d(num_channels, 64, 7, 2, 3)
-        self.classifier = nn.Linear(1024, num_classes, bias = True)
+        self.classifier = nn.Linear(1920, num_classes, bias = True)
         del preloaded
 
     def forward(self, x):
@@ -112,6 +112,44 @@ class DenseNet201(BaseModel):
         out = F.relu(features, inplace = True)
         out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
         out = self.classifier(out)
+        out = F.log_softmax(out, dim = 1)
+        return out
+
+class ResNet152(BaseModel):
+    def __init__(self, num_classes = 1108, num_channels = 6):
+        super().__init__()
+        preloaded = torchvision.models.resnet152(pretrained = True)
+        trained_kernel = preloaded.conv1.weight
+        new_conv = nn.Conv2d(num_channels, 64, 7, 2, 3)
+        with torch.no_grad():
+            new_conv.weight[:,:] = torch.stack([torch.mean(trained_kernel, 1)] * 6, dim = 1)
+        preloaded.conv1 = new_conv
+        num_ftrs = preloaded.fc.in_features
+        preloaded.fc = nn.Linear(num_ftrs, num_classes)
+
+        self.model = preloaded
+
+    def forward(self, x):
+        out = self.model(x)
+        out = F.log_softmax(out, dim = 1)
+        return out
+
+class ResNext101_32x8d(BaseModel):
+    def __init__(self, num_classes = 1108, num_channels = 6):
+        super().__init__()
+        preloaded = torchvision.models.resnet152(pretrained = True)
+        trained_kernel = preloaded.conv1.weight
+        new_conv = nn.Conv2d(num_channels, 64, 7, 2, 3)
+        with torch.no_grad():
+            new_conv.weight[:,:] = torch.stack([torch.mean(trained_kernel, 1)] * 6, dim = 1)
+        preloaded.conv1 = new_conv
+        num_ftrs = preloaded.fc.in_features
+        preloaded.fc = nn.Linear(num_ftrs, num_classes)
+
+        self.model = preloaded
+
+    def forward(self, x):
+        out = self.model(x)
         out = F.log_softmax(out, dim = 1)
         return out
 
